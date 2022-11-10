36aeff86d6129e8c9bbc8819a796e4026c554179
Samaramir <s.ameer.ict@btu.edu.eg>
2022-11-10 02:09:38 -0800

    samer qw

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
  const int itemCount=20;
 class Profilepage extends StatelessWidget {
   const Profilepage({Key? key}) : super(key: key);
-
+//
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
